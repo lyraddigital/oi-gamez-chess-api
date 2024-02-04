9bb1596db182579d8dae159244105a2cf8ebdcae
darylduck <daryl@lyraddigital.com>
2024-02-04 23:54:38 +1100

    Latest

--- lambda/handlers/subscribers/room-events-subscriber/src/index.ts
+++ lambda/handlers/subscribers/room-events-subscriber/src/index.ts
@@ -7,7 +7,7 @@ export const handler = async (
 ): Promise<void> => {
   console.log("event source: ", event.source);
   console.log("event detail type: ", event["detail-type"]);
-  console.log("event detail: ", event.detail?.eventData);
+  console.log("event detail: ", event.detail);
 
   await Promise.resolve();
 };
