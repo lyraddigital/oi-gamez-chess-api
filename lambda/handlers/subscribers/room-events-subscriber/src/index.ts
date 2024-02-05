e1e63f749b01b06f47ef7439f16a6e965d55a538
darylduck <daryl@lyraddigital.com>
2024-02-05 21:16:52 +1100

    Latest

--- lambda/handlers/subscribers/room-events-subscriber/src/index.ts
+++ lambda/handlers/subscribers/room-events-subscriber/src/index.ts
@@ -14,13 +14,7 @@ validateEnvironment();
 export const handler = async (
   event: EventBridgeEvent<any, any>
 ): Promise<void> => {
-  console.log("event source: ", event.source);
-  console.log("event detail type: ", event["detail-type"]);
-  console.log("event detail: ", event.detail);
-
   if (event["detail-type"] === RoomEventTypes.roomCreated) {
-    console.log("Processing room created code.");
-
     const { roomCode } = event.detail as RoomCreatedEvent;
 
     // For now let's just raise the game initialized event
